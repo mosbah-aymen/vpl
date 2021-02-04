5c5099d3e05884b69d2471a05a5f1208c84341dc
mosbah <mosbah.avion12345@gmail.com>
2021-02-04 18:19:05 +0100

    videos.dart//

---
+++ vpl/lib/screens/videos.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class VideoPlayer extends StatefulWidget {
+  static final id ='/';
+  @override
+  _VideoPlayerState createState() => _VideoPlayerState();
+}
+
+class _VideoPlayerState extends State<VideoPlayer> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.grey,
+      ),
+    );
+  }
+}
